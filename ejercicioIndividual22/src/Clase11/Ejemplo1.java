9022ec82db2f14259834ecf0484e7279c4399d9f
Bastian <bastian.gongora@gmail.com>
2023-06-28 21:49:51 -0400

    trabajos individual 22

---
+++ ejercicioIndividual22/src/Clase11/Ejemplo1.java
@@ -0,0 +1,25 @@
+package Clase11;
+
+public class Ejemplo1 {
+    public static void main(String args[]) {
+
+        int numero = 5;
+        String cuento = "En un lugar de la Mancha, de cuyo" +
+                " nombre no quiero acordarme, no ha mucho tiempo que vivía un\n" +
+                " hidalgo de los de lanza en astillero, adarga antigua, rocín flaco\n" +
+                " y galgo corredor. " + "Una olla de algo más vaca que carnero,\n" +
+                " salpicón las más noches, duelos y quebrantos los sábados,\n" +
+                " lantejas los viernes, algún palomino de añadidura los domingos,\n" + "consumían las tres partes de su hacienda.";
+
+    /*El resto della concluían sayo de velarte, calzas de
+    velludo para las fiestas, con sus pantuflos de lo mesmo,
+    y los días de entresemana se honraba con su
+    vellorí de lo más fino */
+
+        int numero2 = 6;
+        System.out.println("El valor de la primera variable es "
+                + numero + " y de la segunda es " + numero2);
+        System.out.println("¿Te cuento un cuento?: \n" + cuento);
+    }
+}
+
